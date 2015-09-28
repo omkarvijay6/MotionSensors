0aac526ee472bce2005ae98f5f2e84697b4b1187
omkarvijay5 <omkarvijay6@gmail.com>
2015-09-28 09:02:52 +0530

    accelerometer readings

--- app/src/main/java/com/example/vijay/motionsensors/SensorsActivity.java
+++ app/src/main/java/com/example/vijay/motionsensors/SensorsActivity.java
@@ -4,13 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorEvent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.content.Context;
+import android.widget.TextView;
 
-public class SensorsActivity extends AppCompatActivity {
+public class SensorsActivity extends AppCompatActivity implements SensorEventListener {
 
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private TextView accelerometer_sensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensors);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -33,5 +46,38 @@ public class SensorsActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+                return;
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            accelerometer_sensor = (TextView) findViewById(R.id.accelerometer_sensor);
+            String acc_readings = String.format("X: %s\nY: %s\nZ: %s", x, y, z);
+            accelerometer_sensor.setText(String.valueOf(acc_readings));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
